4ecf5f86152c8969d805936790713cb116844989
Igor Savin <iselwin@gmail.com>
2023-06-26 15:46:38 +0300

    Migrate to message-queue-toolkit

--- src/modules/users/publishers/PermissionPublisher.ts
+++ src/modules/users/publishers/PermissionPublisher.ts
@@ -1,18 +1,29 @@
-import { AbstractPublisher } from '../../../infrastructure/amqp/AbstractPublisher'
+import { AbstractAmqpPublisher } from '@message-queue-toolkit/amqp'
+
 import type { Dependencies } from '../../../infrastructure/diConfig'
 import type { PERMISSIONS_MESSAGE_TYPE } from '../consumers/userConsumerSchemas'
 import { PERMISSIONS_MESSAGE_SCHEMA } from '../consumers/userConsumerSchemas'
 
-export class PermissionPublisher extends AbstractPublisher<PERMISSIONS_MESSAGE_TYPE> {
+export class PermissionPublisher extends AbstractAmqpPublisher<PERMISSIONS_MESSAGE_TYPE> {
   public static QUEUE_NAME = 'user_permissions'
 
   constructor(dependencies: Dependencies) {
     super(
       {
+        amqpConnection: dependencies.amqpConnection,
+        errorReporter: dependencies.errorReporter,
+        logger: dependencies.logger,
+      },
+      {
         queueName: PermissionPublisher.QUEUE_NAME,
         messageSchema: PERMISSIONS_MESSAGE_SCHEMA,
+        messageTypeField: 'messageType',
+        queueConfiguration: {
+          autoDelete: false,
+          durable: true,
+          exclusive: false,
+        },
       },
-      dependencies,
     )
   }
 }
