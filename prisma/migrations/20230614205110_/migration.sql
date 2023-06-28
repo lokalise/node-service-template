627214ebb84517dc028ebdc362a3af047f67af2c
Igor Savin <iselwin@gmail.com>
2023-06-28 13:55:51 +0300

    Disable foreign keys

--- prisma/migrations/20230614205110_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `age` INTEGER NULL,
-    MODIFY `name` VARCHAR(191) NOT NULL;
