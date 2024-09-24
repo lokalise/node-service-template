9801367fd05b467b56db758b955946318fe3e972
Andrew Ivanov <andrew@lokalise.com>
2024-09-24 13:08:09 +0300

    Fix code format issues

--- scripts/cmd/getUserImportJobs.ts
+++ scripts/cmd/getUserImportJobs.ts
@@ -3,18 +3,18 @@ import { createCliContext, destroyCliContext } from '../utils/cliContextUtils.js
 
 const origin = 'getUserImportJobsCommand'
 const ARGUMENTS_SCHEMA = z.object({
-    queue: z.enum(['active', 'failed', 'delayed', 'completed', 'waiting', 'prioritized']),
+  queue: z.enum(['active', 'failed', 'delayed', 'completed', 'waiting', 'prioritized']),
 })
 type Arguments = z.infer<typeof ARGUMENTS_SCHEMA>
 
 async function run() {
-    const { app, logger, args } = await createCliContext<Arguments>(ARGUMENTS_SCHEMA, origin)
-    const userImportJob = app.diContainer.cradle.userImportJob
+  const { app, logger, args } = await createCliContext<Arguments>(ARGUMENTS_SCHEMA, origin)
+  const userImportJob = app.diContainer.cradle.userImportJob
 
-    const jobs = await userImportJob.getJobsInQueue([args.queue])
-    logger.info(jobs, `${args.queue} jobs`)
+  const jobs = await userImportJob.getJobsInQueue([args.queue])
+  logger.info(jobs, `${args.queue} jobs`)
 
-    await destroyCliContext(app)
+  await destroyCliContext(app)
 }
 
 void run()
