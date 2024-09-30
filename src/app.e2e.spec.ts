6da55557eab578eca9615e36e8cd5950e9429b9a
Igor Savin <iselwin@gmail.com>
2024-09-30 12:25:13 +0300

    Update @scalar/fastify-api-reference

--- src/app.e2e.spec.ts
+++ src/app.e2e.spec.ts
@@ -86,7 +86,7 @@ describe('app', () => {
     it('Returns OpenAPI information (HTML)', async () => {
       const response = await app.inject().get('/documentation').end()
 
-      expect(response.statusCode).toBe(200)
+      expect(response.statusCode).toBe(302)
     })
   })
 
