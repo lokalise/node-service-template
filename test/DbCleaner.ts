2ca8e2abdf0f15077562ad20e04dbdbd0e375521
Daria Carlotta Maino <daria.maino@lokalise.com>
2024-11-22 17:28:23 +0100

    DXP-320 Change how tables are cleaned

--- test/DbCleaner.ts
+++ test/DbCleaner.ts
@@ -1,26 +1,15 @@
-import { sql } from 'drizzle-orm'
+import type { PgTable } from 'drizzle-orm/pg-core'
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+import { post } from '../src/db/schema/post.js'
+import { user } from '../src/db/schema/user.js'
 
-export enum DB_MODEL {
-  User = 'user',
-  Post = 'post',
+export const DB_MODEL: Record<string, PgTable> = {
+  User: user,
+  Post: post,
 }
 
-export async function cleanTables(drizzle: PostgresJsDatabase, modelNames: readonly DB_MODEL[]) {
-  for (const model of modelNames) {
-    const tableName = model.valueOf()
-    const schemaName = getSchemaForModel(model)
-    await drizzle.execute(sql.raw(`delete from "${schemaName}".${tableName}`))
-  }
-}
-
-function getSchemaForModel(model: DB_MODEL): string {
-  switch (model) {
-    case DB_MODEL.User:
-      return 'user'
-    case DB_MODEL.Post:
-      return 'post'
-    default:
-      throw new Error(`No schema defined for model: ${model}`)
+export async function cleanTables(drizzle: PostgresJsDatabase, modelNames: PgTable[]) {
+  for (const modelName of modelNames) {
+    await drizzle.delete(modelName)
   }
 }
