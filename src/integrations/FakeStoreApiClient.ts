7108c0a982a447c97fba6e0c6a76bf33218a58db
Igor Savin <iselwin@gmail.com>
2025-04-13 21:13:28 +0300

    Use MockttpHelper (#1129)

--- src/integrations/FakeStoreApiClient.ts
+++ src/integrations/FakeStoreApiClient.ts
@@ -1,4 +1,5 @@
-import { buildClient, sendGet } from '@lokalise/backend-http-client'
+import { buildGetRoute } from '@lokalise/api-contracts'
+import { buildClient, sendByGetRoute } from '@lokalise/backend-http-client'
 import type { Client } from 'undici'
 import z from 'zod'
 import type { CommonDependencies } from '../infrastructure/CommonModule.js'
@@ -6,6 +7,20 @@ import type { CommonDependencies } from '../infrastructure/CommonModule.js'
 const GET_PRODUCT_RESPONSE_SCHEMA = z.object({
   id: z.number(),
   name: z.string(),
+})
+
+const GET_PRODUCT_PATH_PARAMS_SCHEMA = z.object({
+  productId: z.number(),
+})
+
+export const GET_PRODUCT_CONTRACT = buildGetRoute({
+  successResponseBodySchema: GET_PRODUCT_RESPONSE_SCHEMA,
+  requestPathParamsSchema: GET_PRODUCT_PATH_PARAMS_SCHEMA,
+  description: 'Fake API',
+  responseSchemasByStatusCode: {
+    200: GET_PRODUCT_RESPONSE_SCHEMA,
+  },
+  pathResolver: (pathParams) => `/products/${pathParams.productId}`,
 })
 
 export class FakeStoreApiClient {
@@ -16,16 +31,22 @@ export class FakeStoreApiClient {
   }
 
   async getProduct(productId: number) {
-    const response = await sendGet(this.client, `/products/${productId}`, {
-      requestLabel: 'GET product from FakeStoreAPI',
-      responseSchema: GET_PRODUCT_RESPONSE_SCHEMA,
-      retryConfig: {
-        statusCodesToRetry: [500, 502, 503],
-        retryOnTimeout: false,
-        maxAttempts: 5,
-        delayBetweenAttemptsInMsecs: 200,
+    const response = await sendByGetRoute(
+      this.client,
+      GET_PRODUCT_CONTRACT,
+      {
+        pathParams: { productId },
       },
-    })
+      {
+        requestLabel: 'GET product from FakeStoreAPI',
+        retryConfig: {
+          statusCodesToRetry: [500, 502, 503],
+          retryOnTimeout: false,
+          maxAttempts: 5,
+          delayBetweenAttemptsInMsecs: 200,
+        },
+      },
+    )
 
     return response.result.body
   }
