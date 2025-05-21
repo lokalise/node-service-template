803ad122e0d41a258bbb52644e1bc05278fe2f97
Krzysztof Jamrog <krzysztof.jamrog@lokalise.com>
2025-05-21 16:00:09 +0200

    working authorization step for platformatic/kafka

--- src/modules/users/kafka/PlatformaticKafkaConsumer.ts
+++ src/modules/users/kafka/PlatformaticKafkaConsumer.ts
@@ -1,13 +1,20 @@
 import {
+  Connection,
   Consumer,
+  saslAuthenticateV2,
+  saslHandshakeV1,
   stringDeserializers,
   type Message,
   type MessagesStream,
+  saslScramSha,
 } from '@platformatic/kafka'
 import type { UsersInjectableDependencies } from '../UserModule.js'
 import type { CommonLogger } from '@lokalise/node-core'
 import type { Config } from '../../../infrastructure/config.ts'
 
+/**
+ * NOTE: this is not working yet, it's only a playground for testing connection
+ */
 export class PlatformaticKafkaConsumer {
   private readonly logger: CommonLogger
   private readonly kafkaConfig: Config['kafka']
@@ -21,31 +28,60 @@ export class PlatformaticKafkaConsumer {
   }
 
   async connect() {
-    const clientId = 'my_test_client'
-    /*
-    const response = await saslScramSha.authenticate(
-      {},
-      new Connection(clientId),
+    const clientId = 'crdb.next_gen.test'
+    const groupId = 'crdb.next_gen.test'
+
+    const [kafkaHost, kafkaPort] = (() => {
+      const [host, port] = this.kafkaConfig.brokers[0]!.split(':')
+      return [host, Number(port)] as [string, number]
+    })()
+
+
+    const connection = new Connection(clientId, {
+      tls: {} // Required to use TLS connection strategy
+    })
+    const connectPromise = connection.connect(kafkaHost, kafkaPort)
+    const readyPromise = connection.ready()
+
+    await readyPromise
+    await connectPromise
+
+    this.logger.info({
+      host: kafkaHost,
+      port: kafkaPort,
+      connectionStatus: connection.status,
+    }, 'Kafka connection established')
+
+    const handshakeResponse = await saslHandshakeV1.api.async(connection, 'SCRAM-SHA-512')
+
+    const authResponse = await saslScramSha.authenticate(
+      saslAuthenticateV2.api,
+      connection,
       'SHA-512',
-      'my-test-user',
-      'my-test-secret',
+      this.kafkaConfig.sasl.username,
+      this.kafkaConfig.sasl.password,
     )
-    */
+
+    this.logger.info({
+      connectionStatus: connection.status,
+    }, 'Kafka connection authenticated')
+
+    // TODO Figure out how to use authorization in Consumer
     this.consumer = new Consumer({
       clientId,
-      groupId: 'crdb.next_gen.test',
+      groupId,
       bootstrapBrokers: this.kafkaConfig.brokers,
       deserializers: stringDeserializers,
-      /*protocols: [
-      {
-        name: 'SaslAuthenticate',
-        version: 1,
-        metadata: '',
-      },
-    ],*/
+      // protocols: [
+      // {
+      //   name: 'SaslAuthenticate',
+      //   version: 1,
+      //   metadata: '',
+      // },
+      // ],
     })
 
-    this.stream = await this.consumer.consume({ topics: ['my-topic'] })
+    this.stream = await this.consumer.consume({ topics: ['crdb.next_gen.autopilot.translation.segment'] })
     this.stream.on('data', (message) => {
       this.consume(message)
     })
