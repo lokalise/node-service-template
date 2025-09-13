437eaaef060eceef8aaa8b22da055ce50a49cb45
Igor Savin <iselwin@gmail.com>
2025-09-13 22:32:37 +0300

    fix import

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,6 +1,6 @@
 import { index, integer, pgSchema, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core'
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
-import z from 'zod'
+import { z } from 'zod/v4'
 
 export const userSchema = pgSchema('user')
 
