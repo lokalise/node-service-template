8014412cf08dbfafc2ed2ffd3e55e79005b7d726
Igor Savin <iselwin@gmail.com>
2025-11-11 13:42:54 +0200

    Update OTel (#1404)

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,6 @@
 // this needs to be the very first operation when starting the service
 await import('./otel.ts')
 
-import {
-  executeAndHandleGlobalErrors,
-  globalLogger,
-  resolveGlobalErrorLogObject,
-} from '@lokalise/node-core'
+const server = await import('./serverInternal.ts')
 
-import { ENABLE_ALL } from 'opinionated-machine'
-import { getApp } from './app.ts'
-import { getConfig } from './infrastructure/config.ts'
-
-async function start() {
-  globalLogger.info('Starting application...')
-  const config = executeAndHandleGlobalErrors(getConfig)
-  const app = await getApp({
-    monitoringEnabled: config.app.metrics.isEnabled,
-    healthchecksEnabled: true,
-    jobQueuesEnabled: ENABLE_ALL,
-    enqueuedJobWorkersEnabled: ENABLE_ALL,
-    messageQueueConsumersEnabled: ENABLE_ALL,
-    periodicJobsEnabled: ENABLE_ALL,
-  })
-
-  try {
-    await app.listen({
-      host: config.app.bindAddress,
-      port: config.app.port,
-      listenTextResolver: (address) => {
-        return `node-service-template app listening at ${address}`
-      },
-    })
-  } catch (err) {
-    app.log.error(resolveGlobalErrorLogObject(err))
-    process.exit(1)
-  }
-}
-
-void start()
+await server.startServer()
