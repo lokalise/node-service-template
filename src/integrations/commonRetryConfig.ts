d88c6068628107ae99f80b793afd0d1bc652416a
Igor Savin <iselwin@gmail.com>
2025-11-14 14:27:24 +0200

    Update BE client (#1405)

--- src/integrations/commonRetryConfig.ts
+++ src/integrations/commonRetryConfig.ts
@@ -1,17 +1,9 @@
-import { constants as httpConstants } from 'node:http2'
-
-import type { RetryConfig } from 'undici-retry'
+import { createDefaultRetryResolver, type RetryConfig } from '@lokalise/backend-http-client'
 
 export const commonRetryConfig: RetryConfig = {
-  statusCodesToRetry: [
-    httpConstants.HTTP_STATUS_REQUEST_TIMEOUT,
-    httpConstants.HTTP_STATUS_LOCKED, // 423 Resource is blocked
-    httpConstants.HTTP_STATUS_TOO_MANY_REQUESTS,
-    httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
-    httpConstants.HTTP_STATUS_SERVICE_UNAVAILABLE,
-    httpConstants.HTTP_STATUS_GATEWAY_TIMEOUT,
-  ],
   retryOnTimeout: false,
   maxAttempts: 5,
-  delayBetweenAttemptsInMsecs: 200,
+  delayResolver: createDefaultRetryResolver({
+    baseDelay: 200,
+  }),
 }
