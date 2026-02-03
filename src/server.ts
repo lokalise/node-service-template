e6b35729a677a33528a8e5e24485a89f5e9b4851
Bartosz Drozd <bartosz.drozd@lokalise.com>
2026-02-03 12:48:41 +0100

    chore: [OTel] Do not load modules synchronously

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-// this needs to be the very first operation when starting the service
+import { initOpenTelemetry } from '@lokalise/opentelemetry-fastify-bootstrap'
+import { startServer } from './serverInternal.ts'
 
+// this needs to be the very first operation when starting the service
 if (process.env.OTEL_ENABLED !== 'false') {
-  const { initOpenTelemetry } = await import('@lokalise/opentelemetry-fastify-bootstrap')
   initOpenTelemetry({
     skippedPaths: ['/health', '/ready', '/live', '/metrics', '/'],
   })
 }
 
-const server = await import('./serverInternal.ts')
-
-await server.startServer()
+await startServer()
